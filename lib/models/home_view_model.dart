6022c078e78d649f4e5bd1c17461f67a81cc6f84
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-14 17:09:42 +0300

    added fetch apps on home view model

--- lib/models/home_view_model.dart
+++ lib/models/home_view_model.dart
@@ -1,16 +1,25 @@
 import 'package:yago_tools/core/airtable_client.dart';
 import 'package:mobx/mobx.dart';
+import 'package:yago_tools/models/app_model.dart';
 
 part 'home_view_model.g.dart';
 
 class HomeViewModel = _HomeViewModel with _$HomeViewModel;
 
 abstract class _HomeViewModel with Store {
-
   // Constructor with injected PostClient
   _HomeViewModel(AirtableClient client) : _airtableClient = client;
 
   // Post Client
- late final  AirtableClient _airtableClient;
+  late final AirtableClient _airtableClient;
 
+  // Fetch Apps
+  @action
+  Future<List<AppModel>> fetchApps() async {
+    List<AppModel> apps = [];
+
+    apps = await _airtableClient.getApps();
+
+    return apps;
+  }
 }
