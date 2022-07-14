e23632415b1d85bd365528388c65b03f1655476a
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-14 16:42:51 +0300

    added dot env package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:yago_tools/localizations/codegen_loader.g.dart';
 
 import 'app.dart';
@@ -9,8 +10,13 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
+  // env
+  await dotenv.load(fileName: ".env");
+
   // Locator setup
   setup();
+
+  // Run app
   runApp(
     EasyLocalization(
       path: 'lib/localizations',
